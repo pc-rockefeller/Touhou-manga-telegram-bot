94c4dc3d3beb58f23b62f8320fd8265b32fbb1d9
danila <danil.manuilov.01@gmail.com>
2024-03-07 09:50:58 +0600

    Add gRPC for Python module

--- pypart/main.py
+++ pypart/main.py
@@ -1,14 +1,44 @@
 from parsers import *
 
-if __name__ == '__main__':
-    parsers = []
-    globalObjs = list(globals().items())
+from random import randrange
 
-    for name, obj in globalObjs:
-        print(name, obj)
-        if obj is not BaseParser and isinstance(obj, type) and issubclass(obj, BaseParser):
-            parsers.append(obj())
-    
-    print(parsers[0].parseElementsCount('touhou project'))
-    print(parsers[0].parseElement('touhou project', 22))
-    print(parsers[0].parseElement('touhou project', 23))
+import grpc
+
+from concurrent.futures import ThreadPoolExecutor
+
+from parsers_pb2 import MangaResponse
+from parsers_pb2_grpc import ParsersServicer, add_ParsersServicer_to_server
+
+class ParsersServer(ParsersServicer):
+    def __init__(self) -> None:
+        self.parsers = []
+        globalObjs = list(globals().items())
+        for name, obj in globalObjs:
+            if obj is not BaseParser and isinstance(obj, type) and issubclass(obj, BaseParser):
+                parsers.append(obj())
+
+    def Parse(self, request, context):
+        mangaName = request.mangaName
+        mangaCounts = []
+        mangaCountSum = 0
+        for parser in self.parsers:
+            mangaCounts.append(parser.parseElementsCount(mangaName))
+        mangaCountSum = sum(mangaCounts)
+        randomMangaIndex = randrange(mangaCountSum)
+
+        parserIndex = 0
+        for mangaCount in mangaCounts:
+            if randomMangaIndex < mangaCount:
+                mangaLink = self.parsers[parserIndex].parseElement(mangaName, randomMangaIndex)
+                response = MangaResponse(mangaLink=mangaLink)
+                return response
+            parserIndex += 1
+            randomMangaIndex -= mangaCount
+
+if __name__ == '__main__':
+    server = grpc.server(ThreadPoolExecutor())
+    add_ParsersServicer_to_server(ParsersServer(), server)
+    port = 9999
+    server.add_insecure_port(f'[::]:{port}')
+    server.start()
+    server.wait_for_termination()
